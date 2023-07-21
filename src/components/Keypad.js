3da961af9d15267cc78891ea6f53308543e05207
Nigel <nigel.oketch@student.moringaschool.com>
2023-07-21 03:53:12 +0300

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,30 @@
-// Code Keypad Component Here
+import "@testing-library/jest-dom";
+import { render, fireEvent } from "@testing-library/react";
+import Keypad from "../components/Keypad";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+let container;
 
-export default Keypad;
+beforeEach(() => {
+  container = render(<Keypad />).container;
+});
+
+test("displays one input", () => {
+  const input = container.querySelector("input");
+  expect(input).toBeInTheDocument();
+  expect(input.tagName).toBe("INPUT");
+});
+
+test("displays an input with the right input type", () => {
+  const input = container.querySelector("input");
+  expect(input.type).toBe("password");
+});
+
+test("typing in the input triggers console output", () => {
+  console.log = jest.fn();
+
+  const input = container.querySelector("input");
+  fireEvent.change(input, { target: { value: "123" } });
+
+  expect(console.log).toHaveBeenCalled();
+  expect(console.log.mock.calls[0][0]).toBe("Entering password...");
+});
